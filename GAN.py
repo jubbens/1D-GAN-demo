f2789053d94f6052fec4f0280f978c40f4079c87
Jordan Ubbens <jubbens@gmail.com>
2017-05-08 10:20:52 -0600

    fixed optimizer lists

--- GAN.py
+++ GAN.py
@@ -21,8 +21,8 @@ g_input = tf.random_uniform(shape=(batch_size,1), minval=-5.0, maxval=5.0)
 test_g_input = tf.random_uniform(shape=(num_demo_samples,1), minval=-5.0, maxval=5.0)
 
 # The distribution we are trying to fit
-target_distribution = tf.random_normal(shape=(batch_size,1), mean=3.0, stddev=0.5)
-test_target_distribution = tf.random_normal(shape=(num_demo_samples,1), mean=3.0, stddev=0.5)
+target_distribution = tf.random_normal(shape=(batch_size,1), mean=1.0, stddev=0.5)
+test_target_distribution = tf.random_normal(shape=(num_demo_samples,1), mean=1.0, stddev=0.5)
 
 # Session
 session = tf.Session()
@@ -33,6 +33,8 @@ g_bias_hidden = tf.get_variable('g_bias_1', [hidden_size], initializer=tf.consta
 g_weights_output = tf.get_variable('g_weights_2', shape=[hidden_size, 1], initializer=tf.contrib.layers.xavier_initializer(), dtype=tf.float32)
 g_bias_output = tf.get_variable('g_bias_2', [1], initializer=tf.constant_initializer(0.0), dtype=tf.float32)
 
+generator_vars = [g_weights_hidden, g_bias_hidden, g_weights_output, g_bias_output]
+
 d_weights_hidden = tf.get_variable('d_weights_1', shape=[1, hidden_size], initializer=tf.contrib.layers.xavier_initializer(), dtype=tf.float32)
 d_bias_hidden = tf.get_variable('d_bias_1', [hidden_size], initializer=tf.constant_initializer(0.0), dtype=tf.float32)
 d_weights_hidden_2 = tf.get_variable('d_weights_2', shape=[hidden_size, hidden_size], initializer=tf.contrib.layers.xavier_initializer(), dtype=tf.float32)
@@ -41,6 +43,9 @@ d_weights_hidden_3 = tf.get_variable('d_weights_3', shape=[hidden_size, hidden_s
 d_bias_hidden_3 = tf.get_variable('d_bias_3', [hidden_size], initializer=tf.constant_initializer(0.0), dtype=tf.float32)
 d_weights_output = tf.get_variable('d_weights_4', shape=[hidden_size, 1], initializer=tf.contrib.layers.xavier_initializer(), dtype=tf.float32)
 d_bias_output = tf.get_variable('d_bias_4', [1], initializer=tf.constant_initializer(0.0), dtype=tf.float32)
+
+discriminator_vars = [d_weights_hidden, d_bias_hidden, d_weights_hidden_2, d_bias_hidden_2,
+                      d_weights_hidden_3, d_bias_hidden_3, d_weights_output, d_bias_output]
 
 # Generator network
 def generator(x):
@@ -70,7 +75,7 @@ d_score_generated = discriminator(d_input_generated)
 
 # Optimize discriminator loss
 d_loss = tf.reduce_mean(-tf.log(d_score_real) - tf.log(1 - d_score_generated))
-d_train_op = tf.train.GradientDescentOptimizer(lr).minimize(d_loss)
+d_train_op = tf.train.GradientDescentOptimizer(lr).minimize(d_loss, var_list=discriminator_vars)
 
 # Operations for training generator
 
@@ -79,7 +84,7 @@ d_score_verifying = discriminator(d_input_generated)
 
 # Optimize generator loss
 g_loss = tf.reduce_mean(-tf.log(d_score_verifying))
-g_train_op = tf.train.GradientDescentOptimizer(lr).minimize(g_loss)
+g_train_op = tf.train.GradientDescentOptimizer(lr).minimize(g_loss, var_list=generator_vars)
 
 # For testing
 test_generated = generator(test_g_input)
